fc97c3eead77921a27700a99d02e6db5ae450489
kloi34 <53842237+kloi34@users.noreply.github.com>
2024-03-10 23:26:40 -0700

    Add files via upload

--- plugin.lua
+++ plugin.lua
@@ -1,4 +1,4 @@
--- amoguSV v6.3.1 (25 February 2024)
+-- amoguSV v6.4 (11 March 2024)
 -- by kloi34
 
 -- Many SV tool ideas were stolen from other plugins, so here is credit to those plugins and the
@@ -52,6 +52,15 @@ MAX_ANIMATION_FRAMES = 999          -- maximum number of animation frames allowe
 
 -------------------------------------------------------------------------------------- Menu related
 
+CHINCHILLA_TYPES = {                -- types of chinchilla SVs
+    "Exponential",
+    "Polynomial",
+    "Circular",
+    "Sine Power",
+    "Arc Sine Power",
+    "Inverse Power",
+    "Peter Stock"
+}
 COLOR_THEMES = {                    -- available color themes for the plugin
     "Classic",
     "Strawberry",
@@ -112,10 +121,10 @@ EMOTICONS = {                       -- emoticons to visually clutter the plugin 
     "( C | D )",
     "( w . w )",
     "( ^ w ^ )",
+    "( > . < )",
     "( + x + )",
     "( o _ 0 )",
     "[mwm]",
---    "( > . < )", -- RIP reverse scroll emoticon
     "( v . ^ )",
     "( ^ o v )",
     "( ; A ; )"
@@ -163,6 +172,7 @@ STANDARD_SVS = {                    -- types of standard SVs
     "Circular",
     "Random",
     "Custom",
+    "Chinchilla",
     "Combo"
 }
 STANDARD_SVS_NO_COMBO = {           -- types of standard SVs (excluding combo)
@@ -173,7 +183,8 @@ STANDARD_SVS_NO_COMBO = {           -- types of standard SVs (excluding combo)
     "Sinusoidal",
     "Circular",
     "Random",
-    "Custom"
+    "Custom",
+    "Chinchilla"
 }
 STILL_TYPES = {                     -- types of still displacements
     "No",
@@ -227,30 +238,34 @@ end
 -- Parameters
 --    styleTheme : name of the desired style theme [String]
 function setPluginAppearanceStyles(styleTheme)
+    local boxedStyle = styleTheme == "Boxed" or
+                       styleTheme == "Boxed + Border"
     local cornerRoundnessValue = 5 -- up to 12, 14 for WindowRounding and 16 for ChildRounding
-    local boxed = styleTheme == "Boxed" or
-                  styleTheme == "Boxed + Border"
-    if boxed then cornerRoundnessValue = 0 end
+    if boxedStyle then cornerRoundnessValue = 0 end
     
+    local borderedStyle = styleTheme == "Rounded + Border" or
+                          styleTheme == "Boxed + Border"
     local borderSize = 0
-    local hasBorder = styleTheme == "Rounded + Border" or
-                      styleTheme == "Boxed + Border"
-    if hasBorder then borderSize = 1 end
+    if borderedStyle then borderSize = 1 end
     
-    imgui.PushStyleVar( imgui_style_var.FrameBorderSize,    borderSize              )
-    imgui.PushStyleVar( imgui_style_var.WindowPadding,      { PADDING_WIDTH, 8 }    )
-    imgui.PushStyleVar( imgui_style_var.FramePadding,       { PADDING_WIDTH, 5 }    )
-    imgui.PushStyleVar( imgui_style_var.ItemSpacing,        { 12, 4 }               )
+    imgui.PushStyleVar( imgui_style_var.FrameBorderSize,    borderSize           )
+    imgui.PushStyleVar( imgui_style_var.WindowPadding,      { PADDING_WIDTH, 8 } )
+    imgui.PushStyleVar( imgui_style_var.FramePadding,       { PADDING_WIDTH, 5 } )
+    imgui.PushStyleVar( imgui_style_var.ItemSpacing,        { DEFAULT_WIDGET_HEIGHT / 2 - 1, 4 } )
     imgui.PushStyleVar( imgui_style_var.ItemInnerSpacing,   { SAMELINE_SPACING, 6 } )
-    imgui.PushStyleVar( imgui_style_var.WindowRounding,     cornerRoundnessValue    )
-    imgui.PushStyleVar( imgui_style_var.ChildRounding,      cornerRoundnessValue    )
-    imgui.PushStyleVar( imgui_style_var.FrameRounding,      cornerRoundnessValue    )
-    imgui.PushStyleVar( imgui_style_var.GrabRounding,       cornerRoundnessValue    )
-    imgui.PushStyleVar( imgui_style_var.ScrollbarRounding,  cornerRoundnessValue    )
-    imgui.PushStyleVar( imgui_style_var.TabRounding,        cornerRoundnessValue    )
+    imgui.PushStyleVar( imgui_style_var.WindowRounding,     cornerRoundnessValue )
+    imgui.PushStyleVar( imgui_style_var.ChildRounding,      cornerRoundnessValue )
+    imgui.PushStyleVar( imgui_style_var.FrameRounding,      cornerRoundnessValue )
+    imgui.PushStyleVar( imgui_style_var.GrabRounding,       cornerRoundnessValue )
+    imgui.PushStyleVar( imgui_style_var.ScrollbarRounding,  cornerRoundnessValue )
+    imgui.PushStyleVar( imgui_style_var.TabRounding,        cornerRoundnessValue )
     
-    -- TabBorderSize not working? Doesn't exist? But it's changeable in the style editor demo?
-    -- imgui.PushStyleVar( imgui_style_var.TabBorderSize,      borderSize              ) 
+    -- Doesn't work even though TabBorderSize is changeable in the style editor demo
+    -- imgui.PushStyleVar( imgui_style_var.TabBorderSize,      borderSize           ) 
+    
+    -- https://github.com/ocornut/imgui/issues/7297
+    -- Apparently TabBorderSize doesn't have a imgui_style_var, so it can only be changed with
+    -- imgui.GetStyle() which hasn't worked from my testing in Quaver plugins
 end
 -- Configures the plugin GUI colors
 -- Parameters
@@ -1562,6 +1577,17 @@ function getSettingVars(svType, label)
             finalSVIndex = 2,
             customSV = 1
         }
+    elseif svType == "Chinchilla" then
+        settingVars = {
+            behaviorIndex = 1,
+            chinchillaTypeIndex = 1,
+            chinchillaIntensity = 0.5,
+            avgSV = 1,
+            verticalShift = 0,
+            svPoints = 16,
+            finalSVIndex = 2,
+            customSV = 1
+        }
     elseif svType == "Combo" then
         settingVars = {
             svType1Index = 1,
@@ -2179,6 +2205,23 @@ function circularSettingsMenu(settingVars, skipFinalSV, svPointsForce)
     settingsChanged = chooseSVPoints(settingVars, svPointsForce) or settingsChanged
     settingsChanged = chooseFinalSV(settingVars, skipFinalSV) or settingsChanged
     settingsChanged = chooseNoNormalize(settingVars) or settingsChanged
+    return settingsChanged
+end
+-- Creates the menu for chinchilla SV settings
+-- Returns whether settings have changed or not [Boolean]
+-- Parameters
+--    settingVars   : list of setting variables for this chinchilla menu [Table]
+--    skipFinalSV   : whether or not to skip choosing the final SV [Boolean]
+--    svPointsForce : number of SV points to force [Int or nil]
+function chinchillaSettingsMenu(settingVars, skipFinalSV, svPointsForce)
+    local settingsChanged = false
+    settingsChanged = chooseSVBehavior(settingVars) or settingsChanged
+    settingsChanged = chooseChinchillaType(settingVars) or settingsChanged
+    settingsChanged = chooseChinchillaIntensity(settingVars) or settingsChanged
+    settingsChanged = chooseAverageSV(settingVars) or settingsChanged
+    settingsChanged = chooseConstantShift(settingVars, 0) or settingsChanged
+    settingsChanged = chooseSVPoints(settingVars, svPointsForce) or settingsChanged
+    settingsChanged = chooseFinalSV(settingVars, skipFinalSV) or settingsChanged
     return settingsChanged
 end
 -- Creates the menu for random SV settings
@@ -2852,6 +2895,8 @@ function showSettingsMenu(currentSVType, settingVars, skipFinalSV, svPointsForce
         return randomSettingsMenu(settingVars, skipFinalSV, svPointsForce)
     elseif currentSVType == "Custom" then
         return customSettingsMenu(settingVars, skipFinalSV, svPointsForce)
+    elseif currentSVType == "Chinchilla" then
+        return chinchillaSettingsMenu(settingVars, skipFinalSV, svPointsForce)
     elseif currentSVType == "Combo" then
         return comboSettingsMenu(settingVars)
     end
@@ -3404,6 +3449,15 @@ function exportPlaceSVButton(globalVars, menuVars, settingVars)
         table.insert(exportList, settingVars.svPoints)
         table.insert(exportList, settingVars.finalSVIndex)
         table.insert(exportList, settingVars.customSV)
+    elseif currentSVType == "Chinchilla" then
+        table.insert(exportList, settingVars.behaviorIndex)
+        table.insert(exportList, settingVars.chinchillaTypeIndex)
+        table.insert(exportList, settingVars.chinchillaIntensity)
+        table.insert(exportList, settingVars.avgSV)
+        table.insert(exportList, settingVars.verticalShift)
+        table.insert(exportList, settingVars.svPoints)
+        table.insert(exportList, settingVars.finalSVIndex)
+        table.insert(exportList, settingVars.customSV)
     elseif currentSVType == "Stutter" then
         table.insert(exportList, settingVars.startSV)
         table.insert(exportList, settingVars.endSV)
@@ -3485,6 +3539,7 @@ function importPlaceSVButton(globalVars)
     local circularSVType    = currentSVType == "Circular"
     local randomSVType      = currentSVType == "Random"
     local customSVType      = currentSVType == "Custom"
+    local chinchillaSVType  = currentSVType == "Chinchilla"
     local stutterSVType     = currentSVType == "Stutter"
     local tpStutterSVType   = currentSVType == "Teleport Stutter"
     
@@ -3509,6 +3564,7 @@ function importPlaceSVButton(globalVars)
     if circularSVType    then menuVars.svTypeIndex = 6 end
     if randomSVType      then menuVars.svTypeIndex = 7 end
     if customSVType      then menuVars.svTypeIndex = 8 end
+    if chinchillaSVType  then menuVars.svTypeIndex = 9 end
     
     if stutterSVType     then menuVars.svTypeIndex = 1 end
     if tpStutterSVType   then menuVars.svTypeIndex = 2 end
@@ -3590,6 +3646,15 @@ function importPlaceSVButton(globalVars)
         settingVars.svPoints = table.remove(settingsTable)
         settingVars.selectedMultiplierIndex = table.remove(settingsTable)
         settingVars.svMultipliers = settingsTable
+    elseif chinchillaSVType then
+        settingVars.behaviorIndex = table.remove(settingsTable, 1)
+        settingVars.chinchillaTypeIndex = table.remove(settingsTable, 1)
+        settingVars.chinchillaIntensity = table.remove(settingsTable, 1)
+        settingVars.avgSV = table.remove(settingsTable, 1)
+        settingVars.verticalShift = table.remove(settingsTable, 1)
+        settingVars.svPoints = table.remove(settingsTable, 1)
+        settingVars.finalSVIndex = table.remove(settingsTable, 1)
+        settingVars.customSV = table.remove(settingsTable, 1)
     end
     
     if stutterSVType then
@@ -4550,7 +4615,7 @@ end
 -- Creates a simple action menu + button that does SV things
 -- Parameters
 --    buttonText   : text on the button that appears [String]
---    minimumNotes : minimum number of notes to select before the action button appears [String]
+--    minimumNotes : minimum number of notes to select before the action button appears [Int/Float]
 --    actionfunc   : function to execute once button is pressed [Function]
 --    globalVars   : list of variables used globally across all menus [Table]
 --    menuVars     : list of variables used for the current menu [Table]
@@ -4641,6 +4706,26 @@ function chooseBezierPoints(settingVars)
     local x2Changed = (oldSecondPoint[1] ~= settingVars.x2)
     local y2Changed = (oldSecondPoint[2] ~= settingVars.y2)
     return x1Changed or y1Changed or x2Changed or y2Changed
+end
+-- Lets you choose the chinchilla SV intensity
+-- Returns whether or not the intensity changed [Boolean]
+-- Parameters
+--    settingVars : list of variables used for the current menu [Table]
+function chooseChinchillaIntensity(settingVars)
+    local oldIntensity = settingVars.chinchillaIntensity
+    local _, newIntensity = imgui.SliderFloat("Intensity##chinchilla", oldIntensity, 0, 10, "%.3f")
+    helpMarker("Ctrl + click slider to input a specific number")
+    settingVars.chinchillaIntensity = clampToInterval(newIntensity, 0, 727)
+    return oldIntensity ~= settingVars.chinchillaIntensity
+end
+-- Lets you choose the chinchilla SV type
+-- Returns whether or not the SV type changed [Boolean]
+-- Parameters
+--    settingVars : list of variables used for the current menu [Table]
+function chooseChinchillaType(settingVars)
+    local oldIndex = settingVars.chinchillaTypeIndex
+    settingVars.chinchillaTypeIndex = combo("Chinchilla Type", CHINCHILLA_TYPES, oldIndex)
+    return oldIndex ~= settingVars.chinchillaTypeIndex 
 end
 -- Lets you choose the color theme of the plugin
 -- Parameters
@@ -5519,6 +5604,8 @@ function generateSVMultipliers(svType, settingVars, interlaceMultiplier)
                                    settingVars.verticalShift, settingVars.dontNormalize)
     elseif svType == "Custom" then
         multipliers = generateCustomSet(settingVars.svMultipliers)
+    elseif svType == "Chinchilla" then
+        multipliers = generateChinchillaSet(settingVars)
     elseif svType == "Combo" then
         local svType1 = STANDARD_SVS[settingVars.svType1Index]
         local settingVars1 = getSettingVars(svType1, "Combo1")
@@ -5736,6 +5823,86 @@ function generateCircularSet(behavior, arcPercent, avgValue, verticalShift, numV
     table.insert(circularSet, avgValue)
     return circularSet
 end
+-- Returns a set of chinchilla values [Table]
+-- Parameters
+--    settingVars : list of variables used for the current menu [Table]
+function generateChinchillaSet(settingVars)
+    if settingVars.svPoints == 1 then return {settingVars.avgSV, settingVars.avgSV} end
+    
+    local avgValue = settingVars.avgSV - settingVars.verticalShift
+    local chinchillaSet = {}
+    local percents = generateLinearSet(0, 1, settingVars.svPoints + 1)
+    local newPercents = {}
+    for i = 1, #percents do
+        local currentPercent = percents[i]
+        local newPercent = scalePercent(settingVars, currentPercent) --
+        table.insert(newPercents, newPercent)
+    end
+    local numValues = settingVars.svPoints
+    for i = 1, numValues do
+        local distance = newPercents[i + 1] - newPercents[i]
+        local slope = distance * numValues
+        chinchillaSet[i] = slope
+    end
+    normalizeValues(chinchillaSet, avgValue, true)
+    for i = 1, #chinchillaSet do
+        chinchillaSet[i] = chinchillaSet[i] + settingVars.verticalShift
+    end
+    table.insert(chinchillaSet, settingVars.avgSV)
+    return chinchillaSet
+end
+-- Scales a percent value based on the selected scale type
+-- Scaling graphs on Desmos: https://www.desmos.com/calculator/z00xjksfnk
+-- Parameters
+--    settingVars : list of variables used for the current menu [Table]
+--    percent     : percent value to scale [Int/Float]
+function scalePercent(settingVars, percent)
+    local behaviorType = SV_BEHAVIORS[settingVars.behaviorIndex]
+    local slowDownType = behaviorType == "Slow down"
+    local workingPercent = percent
+    if slowDownType then workingPercent = 1 - percent end
+    local newPercent
+    local a = settingVars.chinchillaIntensity
+    local scaleType = CHINCHILLA_TYPES[settingVars.chinchillaTypeIndex]
+    if scaleType == "Exponential" then
+        local exponent = a * (workingPercent - 1)
+        newPercent = (workingPercent * math.exp(exponent))
+    elseif scaleType == "Polynomial" then
+        local exponent = a + 1
+        newPercent = workingPercent ^ exponent
+    elseif scaleType == "Circular" then
+        if a == 0 then return percent end
+        
+        local b = 1 / (a ^ (a + 1))
+        local radicand = (b + 1) ^ 2 + b ^ 2 - (workingPercent + b) ^ 2
+        newPercent = b + 1 - math.sqrt(radicand)
+    elseif scaleType == "Sine Power" then
+        local exponent = math.log(a + 1)
+        local base = math.sin(math.pi * (workingPercent - 1) / 2) + 1
+        newPercent = workingPercent * (base ^ exponent)
+    elseif scaleType == "Arc Sine Power" then
+        local exponent = math.log(a + 1)
+        local base = 2 * math.asin(workingPercent) / math.pi
+        newPercent = workingPercent * (base ^ exponent)
+    elseif scaleType == "Inverse Power" then
+        local denominator = 1 + (workingPercent ^ -a)
+        newPercent = 2 * workingPercent / denominator
+    elseif "Peter Stock" then
+        --[[
+        Algorithm based on a modified version of Peter Stock's StackExchange answer.
+        Peter Stock (https://math.stackexchange.com/users/1246531/peter-stock)
+        SmoothStep: Looking for a continuous family of interpolation functions
+        URL (version: 2023-11-04): https://math.stackexchange.com/q/4800509
+        --]]
+        if a == 0 then return percent end
+        
+        local c = a / (1 - a)
+        newPercent = (workingPercent ^ 2) * (1 + c) / (workingPercent + c)
+    end
+    if slowDownType then newPercent = 1 - newPercent end
+    return clampToInterval(newPercent, 0, 1)
+end
+-- Returns the average of two numbers [Int/Float]
 -- Returns a modified set of random values [Table]
 -- Parameters
 --    values        : list of random values [Table]
